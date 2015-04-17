bcd0f85774b913d60522a1f77a40ddac6a7a0df4
Liz <lizroblec@gmail.com>
2015-04-17 00:27:04 -0500

    Nuevo
    Añadida

---
+++ Tarea4_DeteccionCirculos/STarea2.py
@@ -0,0 +1,72 @@
+#Se genera un color de manera aleatoria
+import random
+def color(a):
+    R= int(random.random()*a)
+    G= int(random.random()*a)
+    B= int(random.random()*a)
+    return R,G,B
+
+#Busqueda por profundidad
+def DFS(Binaria,fila1,columna1):
+    fila, columna= Binaria.shape
+    marca= 254
+    frecuencias= dict()
+    CantPixeles= 0
+    pixeles=[]
+    Contenedor=[]
+    Contenedor.append((fila1,columna1))
+    original= Binaria[(fila1,columna1)]
+    while len(Contenedor) > 0:
+        (fila1, columna1)= Contenedor.pop(0)
+        actual = Binaria[fila1, columna1]
+        if actual == original or actual == marca:
+            for dx in [-1, 0, 1]:
+                for dy in [-1, 0, 1]:
+                    i, j = (fila1 + dx, columna1 + dy)
+                    if i >= 0 and i < fila and j >= 0 and j < columna:
+                        contenido = Binaria[i, j]
+                        if contenido == original:
+                            Binaria[i, j]= marca
+                            pixeles.append((i,j))
+                            CantPixeles += 1
+                            Contenedor.append((i, j))
+    return pixeles, CantPixeles
+
+#Generar la caja envolvente
+def CajaEnvolvente(pixeles):
+    fila=[]
+    columna=[]
+    CajaEnvolvente=[]
+    for i in pixeles:
+        fila.append(i[0])
+        columna.append(i[1])
+    Mfila=max(fila)
+    mfila=min(fila)
+    Mcolumna=max(columna)
+    mcolumna=min(columna)
+    for i in range(mcolumna, Mcolumna):
+        CajaEnvolvente.append((mfila,i))
+    for i in range(mfila,Mfila+1):
+        CajaEnvolvente.append((i,Mcolumna))
+    for i in range(mfila+1,Mfila):
+        CajaEnvolvente.append((i,mcolumna))
+    for i in range(mcolumna,Mcolumna):
+        CajaEnvolvente.append((Mfila,i))
+    CentroXB=mcolumna+int((Mcolumna-mcolumna)/2)
+    CentroYB=mfila+int((Mfila-mfila)/2)
+    return CajaEnvolvente,CentroXB,CentroYB
+
+def CentroMasa(pixeles):
+    CentroY=[]
+    CentroX=[]
+    for i in pixeles:
+        CentroY.append(i[0])
+        CentroX.append(i[1])
+    CentroY= sum(CentroY)/len(pixeles)
+    CentroX= sum(CentroX)/len(pixeles)
+    return CentroY,CentroX
+
+
+
+                                         
+                                
---
+++ Tarea4_DeteccionCirculos/STarea3.py
@@ -0,0 +1,65 @@
+def Orientacion(ang,pixeles):
+    import math
+    DAngulos=dict()
+    theta=[]
+    for i in pixeles:
+        theta= int(90+ang[i])
+        theta.append(theta)
+        if theta==360:
+            theta=0
+        if theta not in DAngulo:
+            DAngulos[theta]=1
+        else:
+            DAngulos[theta]+=1
+    valores=list(DAngulos.values())
+    claves=list(DAngulos.keys())
+    a=valores.index(max(valores))
+    return claves[a]
+
+def Regresor(fila,columna,CentroYB,CentroXB,Orientacion):
+    import math
+    x=1
+    y=1
+    x2=0
+    y2=0
+    radio=0
+    radio2=0
+    puntos=[]
+    while(y<fila-1 and y>=1 and x<columna-1 and x>=1):
+                x=int(round(CentroXB-radio*math.cos(math.radians(Orientacion))))
+                y=int(round(CentroYB+radio*math.sin(math.radians(Orientacion))))
+                x2=int(round(CentroXB-radio2*math.cos(math.radians(Orientacion))))
+                y2=int(round(CentroYB+radio2*math.sin(math.radians(Orientacion))))
+                if x<columna-1 and y<fila-1:
+                    puntos.append((y,x))
+                    puntos.append((y2,x2))
+                radio-=1
+                radio2+=1
+            
+    return puntos
+
+
+def CajaEnvolvente2(pixeles):
+    import math
+    fila=[]
+    columna=[]
+    CajaEnvolvente=[]
+    for i in pixeles:
+        fila.append(i[0])
+        columna.append(i[1])
+    Mfila=max(fila)
+    mfila=min(fila)
+    Mcolumna=max(columna)
+    mcolumna=min(columna)
+    for i in range(mcolumna, Mcolumna):
+        CajaEnvolvente.append((mfila,i))
+    for i in range(mfila,Mfila+1):
+        CajaEnvolvente.append((i,Mcolumna))
+    for i in range(mfila+1,Mfila):
+        CajaEnvolvente.append((i,mcolumna))
+    for i in range(mcolumna,Mcolumna):
+        CajaEnvolvente.append((Mfila,i))
+    CentroXB= mcolumna+int((Mcolumna-mcolumna)/2)
+    CentroYB= mfila+int((Mfila-mfila)/2)
+    magnitud= math.sqrt((Mcolumna-mcolumna)**2 + (Mfila-mfila)**2)
+    return CajaEnvolvente,CentroXB,CentroYB, magnitud
